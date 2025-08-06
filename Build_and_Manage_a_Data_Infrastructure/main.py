1132d7a351f7b9924711af58cc38288c2131530b
Martin BONNARD <bonnard.martin@gmail.com>
2025-08-06 11:43:35 +0200

    ajout ExctractCitiesLocation

--- Build_and_Manage_a_Data_Infrastructure/main.py
+++ Build_and_Manage_a_Data_Infrastructure/main.py
@@ -1,7 +1,7 @@
 # Main script
 # On peut lancer ce script dans le venv et ainsi faire les extractions de données, leur traitement et enregistrement
 import os
-from data_extraction.wheather import LoadWeather
+from data_extraction.wheather import LoadWeather, ExtractCitiesLocation
 from scrapping.scrapBooking import Crawl
 from etl_sql.insertData import DataInsertion
 
@@ -13,13 +13,17 @@ if __name__ == "__main__":
 
     print("Lancement du Script ETL")
     if not os.path.exists('./data/wheather.csv'):
+        print("Récupération des Latitudes et logitudes")
+        ExtractCitiesLocation()
+        
+    if not os.path.exists('./data/wheather.csv'):
         print("Lancement de la récupération de la météo")
         LoadWeather()
         print("Fin de la récupération de la météo")
     
     print("Lancement du Scrapping")
 
-    
+
     # On pourra relancer le script autant de fois que nécessaire pour avoir des hotels dans toutes les villes car de temps à autres les
     # les hotels ne sont pas scrappés tout de suite.
     # On pourrait mettre en place un CrawlerRunner plutot que CrawlerProcess car le pas possible de relancer plusieur fois CrawlerProcess
