e89cf9ae09ebf528f9c6e6874242fe62bf3aa32c
Martin BONNARD <bonnard.martin@gmail.com>
2025-08-09 23:48:44 +0200

    travail sur spider info supp a recupérer

--- Build_and_Manage_a_Data_Infrastructure/scrapping/crawelerHotelsLatLong.py
+++ Build_and_Manage_a_Data_Infrastructure/scrapping/crawelerHotelsLatLong.py
@@ -7,20 +7,40 @@ import os
 class CrawlerLatLongHotels(scrapy.Spider):
     def __init__(self, name = 'Hotel2', **kwargs):
         super().__init__(name, **kwargs)
-        
-
-class RecupLatLong():
-    def __init__(self):
         self.urls = []
         if not os.path.isfile('./data/hotels.csv') : 
             return
-        # self.data = pd.read_csv('./data/hotels.csv')
-        # print(self.data)
         with open('./data/hotels.csv', 'r',encoding='utf-8') as f:
             lines = f.readlines()
             for line in lines:
                 self.urls.append(line.split(',')[4])
-                
-            for a in self.urls:
-                print(a)
+        self.urls = self.urls[1:]
+
+    async def start(self):
+        for url in self.urls:
+            request = scrapy.Request(
+                url=url,
+                callback=self.parse,
+                headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/115.0 Safari/537.36"}
+                )
+            yield request
+        yield
+
+    async def parse(self, response):
+        lat_lon  = response.xpath('//a[@id="map_trigger_header_pin"]/@data-atlas-latlng').get()
+        print(lat_lon)
+
+class RecupLatLong():
+    def __init__(self):
+        self.lancerCrawler()
+        return
+    def lancerCrawler(self):
+        spider = CrawlerLatLongHotels
+        process = CrawlerProcess(settings={
+            "LOG_LEVEL": "ERROR",
+            "DOWNLOAD_DELAY": 1,
+        })
+        process.crawl(spider)
+        process.start()
+
                 
