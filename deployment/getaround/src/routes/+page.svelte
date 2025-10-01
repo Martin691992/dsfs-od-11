c4c7d5c12e869ead617638febd91a33779b2e32e
Martin BONNARD <bonnard.martin@gmail.com>
2025-10-01 18:44:53 +0200

    maj ui svelte 01/10

--- deployment/getaround/src/routes/+page.svelte
+++ deployment/getaround/src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div>
+    <h1>Bienvenue sur votre dashboard</h1>
+
+</div>
+<style>
+    div{
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        margin-top: 4em;
+        align-items: center;
+        h1{
+            font-family: "Poppins";
+            font-size: clamp(1em,3em,3vw);
+			color: rgba(0, 0, 0, 0.7);
+        }
+    }
+</style>
